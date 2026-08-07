bbaeb3760b006b888eac9c3b4bd9c2468eafcbff
Kaiseela <sharonnekaitek@icloud.com>
2026-08-07 15:31:59 +0100

    Redirect homepage to English site

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,5 @@
-import About from "./components/About";
-import Books from "./components/Books";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import GivingBack from "./components/GivingBack";
-import Hero from "./components/Hero";
-import Mission from "./components/Mission";
-import Photography from "./components/Photography";
-import MeetKai from "./components/MeetKai";
-import HeroV2 from "./components/HeroV2";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main>
-      <HeroV2 />
-      <MeetKai />
-      <About />
-      <Mission />
-      <Books />
-      <Photography />
-      <GivingBack />
-      <Contact />
-      <Footer />
-    </main>
-  );
+  redirect("/en");
 }
